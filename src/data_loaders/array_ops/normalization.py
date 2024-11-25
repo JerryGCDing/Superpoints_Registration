b5aacc6edeec964c651805cd038ff69df5669b93
Jerry Ding <13386637338@qq.com>
2024-11-25 08:57:46 -0500

    Update normalization.py

--- src/data_loaders/array_ops/normalization.py
+++ src/data_loaders/array_ops/normalization.py
@@ -7,23 +7,25 @@ def min_max_norm(
         *,
         axis: int = 0,
         min_val: Optional[Union[np.ndarray, Tuple, List]] = None,
-        max_val: Optional[Union[np.ndarray, Tuple, List]] = None
+        max_val: Optional[Union[np.ndarray, Tuple, List]] = None,
+        eps: Optional[float] = 1e-8
 ):
     a = a.astype(np.float32)
     min_val = np.min(a, axis=axis, keepdims=True) if min_val is None else np.asarray(min_val)
     max_val = np.max(a, axis=axis, keepdims=True) if max_val is None else np.asarray(max_val)
 
-    norm = (a - min_val) / (max_val - min_val)
+    norm = (a - min_val) / (max_val - min_val + eps)
     return norm
+
 
 def center_shift(points, apply_z=True):
     x_min, y_min, z_min = points.min(axis=0)
     x_max, y_max, _ = points.max(axis=0)
-    
+
     if apply_z:
         shift = [(x_min + x_max) / 2, (y_min + y_max) / 2, z_min]
     else:
         shift = [(x_min + x_max) / 2, (y_min + y_max) / 2, 0]
 
     points -= shift
-    return points    
+    return points

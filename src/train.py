9a37825b63298040d12d28c256d2c176c7c29f21
Jerry Ding <13386637338@qq.com>
2025-01-31 01:22:29 -0500

    suppress future warning

--- src/train.py
+++ src/train.py
@@ -11,6 +11,8 @@ from torch.distributed import init_process_group
 import os
 import torch
 import json
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from utils.comm import *
 
